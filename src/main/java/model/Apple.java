c114d76c765e7af87641b866307bd13e072b9a6c
twicy <polygalovakate@gmail.com>
2025-05-04 20:49:14 +0300

    Initial commit

---
+++ src/main/java/model/Apple.java
@@ -0,0 +1,23 @@
+package model;
+
+import model.constants.Discount;
+import model.constants.Colour;
+
+public class Apple extends Food{
+    private String colour;
+
+    public Apple(int amount, double price, String colour) {
+        this.amount = amount;
+        this.price = price;
+        this.colour = colour;
+        this.isVegetarian = true;
+    }
+
+    @Override
+    public double getDiscount() {
+        if (colour == Colour.RED) {
+            return Discount.APPLE_RED_DISCOUNT;
+            }
+        return 0;
+    }
+}
---
+++ src/main/java/service/ShoppingCart.java
@@ -0,0 +1,55 @@
+package service;
+
+import model.Food;
+
+public class ShoppingCart {
+
+    private Food[] items;
+
+    public ShoppingCart(Food[] items) {
+        this.items = items;
+    }
+
+    public double getTotalPriceWithoutDiscount() {
+        double total = 0;
+        for (int i = 0; i < items.length; i++) {
+            Food item = items[i];
+            double itemPrice = item.getAmount() * item.getPrice();
+            total = total + itemPrice;
+        }
+        return total;
+    }
+
+    public double getTotalPriceWithDiscount() {
+        double total = 0;
+
+        for (int i = 0; i < items.length; i++) {
+            Food item = items[i];
+            double price = item.getPrice();
+            int amount = item.getAmount();
+            double discount = item.getDiscount();
+
+            double discountedPrice = price - (price * discount / 100);
+            double itemTotal = amount * discountedPrice;
+
+            total = total + itemTotal;
+        }
+
+        return total;
+    }
+
+    public double getVegetarianTotalPriceWithoutDiscount() {
+        double total = 0;
+
+        for (int i = 0; i < items.length; i++) {
+            Food item = items[i];
+            if (item.isVegetarian()) {
+                double itemPrice = item.getAmount() * item.getPrice();
+                total = total + itemPrice;
+            }
+
+        }
+        return total;
+    }
+
+}
