c8b2280a8c779681cd682dd73f5045578d1cdbf4
twicy <polygalovakate@gmail.com>
2025-05-06 13:52:27 +0300

    начальная версия проекта Sprint 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,8 +16,6 @@ public class Main {
 
         System.out.println("Общая сумма товаров без скидки: " + cart.getTotalPriceWithoutDiscount());
         System.out.println("Общая сумма товаров со скидкой: " + cart.getTotalPriceWithDiscount());
-        System.out.println("Сумма всех вегетарианских продуктов без скидки: " + cart.getVegetarianTotalPriceWithoutDiscount() + ".");
-        System.out.println("Сумма всех вегетарианских продуктов без скидки: " + cart.getVegetarianTotalPriceWithoutDiscount() + ".");
         System.out.println("Сумма всех вегетарианских продуктов без скидки: " + cart.getVegetarianTotalPriceWithoutDiscount());
 
     }
