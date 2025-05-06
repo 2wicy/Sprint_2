27c43e25bbbcbedae1b5f886d312910523772a2c
twicy <polygalovakate@gmail.com>
2025-05-06 13:49:32 +0300

    Ignore .DS_Store file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,6 +17,8 @@ public class Main {
         System.out.println("Общая сумма товаров без скидки: " + cart.getTotalPriceWithoutDiscount());
         System.out.println("Общая сумма товаров со скидкой: " + cart.getTotalPriceWithDiscount());
         System.out.println("Сумма всех вегетарианских продуктов без скидки: " + cart.getVegetarianTotalPriceWithoutDiscount() + ".");
+        System.out.println("Сумма всех вегетарианских продуктов без скидки: " + cart.getVegetarianTotalPriceWithoutDiscount() + ".");
+        System.out.println("Сумма всех вегетарианских продуктов без скидки: " + cart.getVegetarianTotalPriceWithoutDiscount());
 
     }
 }
